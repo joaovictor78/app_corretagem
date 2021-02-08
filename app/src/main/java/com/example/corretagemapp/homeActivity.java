32e245c7eada8ed6eefa4e8c476d6aa17b29aea2
JVictor <joaovictorb.dasilva2003@gmail.com>
2021-02-07 21:53:10 -0300

    navegação de telas

--- app/src/main/java/com/example/corretagemapp/homeActivity.java
+++ app/src/main/java/com/example/corretagemapp/homeActivity.java
@@ -1,24 +1,57 @@
 package com.example.corretagemapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.corretagemapp.models.Corretora;
 import com.example.corretagemapp.models.Corretoras;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
+
 public class homeActivity extends AppCompatActivity {
-     private CorretoraAdapter corretoraAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
-        corretoraAdapter = new CorretoraAdapter(new ArrayList<>(Corretoras.listCorretoras()));
-        RecyclerView recyclerView = findViewById(R.id.rowlistview);
-        recyclerView.setAdapter(corretoraAdapter);
-
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavView);
+        bottomNav.getMenu().getItem(1).setEnabled(false);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Home()).commit();
+        }
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.home:
+                            selectedFragment = new Home();
+                            break;
+                        case R.id.employees:
+                            selectedFragment = new Funcionarios();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
+
 }
