a51675f3738cdee04b44e27016d3dd077831ce3d
JVictor <joaovictorb.dasilva2003@gmail.com>
2021-02-09 00:20:04 -0300

    app

---
+++ app/src/main/java/com/example/corretagemapp/ValorTotalCotacao.java
@@ -0,0 +1,14 @@
+package com.example.corretagemapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ValorTotalCotacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valor_total_cotacao);
+    }
+}
