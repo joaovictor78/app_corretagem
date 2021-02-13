f35ce16e7ae758835ce113e3f7d836eaeba08e0e
JVictor <joaovictorb.dasilva2003@gmail.com>
2021-02-13 11:45:16 -0400

    Criação do seletor de eventos calendario

--- app/src/main/java/com/example/corretagemapp/calendar.java
+++ app/src/main/java/com/example/corretagemapp/calendar.java
@@ -6,11 +6,13 @@ import android.app.ActionBar;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.applandeo.materialcalendarview.CalendarUtils;
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -20,6 +22,7 @@ public class calendar extends AppCompatActivity {
     private CalendarView calendarView;
     private List<EventDay> myEvents = new ArrayList<>();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +30,15 @@ public class calendar extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         calendarView = (CalendarView) findViewById(R.id.calendarView);
         Calendar calendarD = Calendar.getInstance();
-        Drawable text = CalendarUtils.getDrawableText(getApplicationContext(), "Joao Victorrrr",  Typeface.DEFAULT_BOLD, android.R.color.black, 15);
-        myEvents.add(new EventDay(calendarD, text));
+        myEvents.add(new EventDay(calendarD, R.drawable.cicle_avatar));
         calendarView.setEvents(myEvents);
+        calendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                if(myEvents.contains(eventDay))
+                Log.i("Estee", "È o evento bixo");
+            }
+        });
     }
 
 }
