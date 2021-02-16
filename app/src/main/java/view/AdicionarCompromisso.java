04fb2bef1d2beb366cb94631624100a235704d33
JVictor <joaovictorb.dasilva2003@gmail.com>
2021-02-16 18:06:22 -0400

    adicionado mascara para a data e hora

--- app/src/main/java/view/AdicionarCompromisso.java
+++ app/src/main/java/view/AdicionarCompromisso.java
@@ -20,7 +20,8 @@ public class AdicionarCompromisso extends AppCompatActivity {
    EditText hora;
    EditText descricao;
    Button saveCompromisso;
-   private String lastCharacter = "";
+   private String lastCharacterDate = "";
+    private String lastCharacterHora = "";
    private String result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,24 +37,50 @@ public class AdicionarCompromisso extends AppCompatActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
                 Integer lengthData = data.getText().length();
                 if(lengthData > 1){
-                    lastCharacter = data.getText().toString().substring(lengthData - 1);
+                    lastCharacterDate = data.getText().toString().substring(lengthData - 1);
                 }
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Integer lengthData = data.getText().length();
+                Integer lengthData = data.getText().toString().length();
                 if(lengthData == 2){
-                    if(!lastCharacter.equals("/")) {
+                    if(!lastCharacterDate.equals("/")) {
                         data.getText().append("/");
                     } else{
                         data.getText().delete(lengthData - 1, lengthData);
                     }
                 } else if(lengthData == 5){
-                    if(!lastCharacter.equals("/")) {
+                    if(!lastCharacterDate.equals("/")) {
                         data.getText().append("/");
                     } else{
                         data.getText().delete(lengthData - 1, lengthData);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        hora.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Integer lengthHora = hora.getText().length();
+                if(lengthHora > 1){
+                    lastCharacterHora = hora.getText().toString().substring(lengthHora - 1);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Integer lengthHora = hora.getText().toString().length();
+                if(lengthHora == 2){
+                    if(!lastCharacterHora.equals(":")) {
+                        hora.getText().append(":");
+                    } else{
+                        hora.getText().delete(lengthHora - 1, lengthHora);
                     }
                 }
             }
