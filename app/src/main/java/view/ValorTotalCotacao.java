e636d7532ea4fe426e6e35eefeed20cdfe0711b4
Marcelo <marcelof.camargo6912@gmail.com>
2021-02-17 11:56:00 -0400

    icone adcionado ao app

--- app/src/main/java/view/ValorTotalCotacao.java
+++ app/src/main/java/view/ValorTotalCotacao.java
@@ -1,7 +1,9 @@
 package view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.app.ActionBar;
@@ -16,10 +18,14 @@ import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.corretagemapp.R;
 import com.example.corretagemapp.models.CotacaoModel;
+
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -45,10 +51,10 @@ public class ValorTotalCotacao extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 File result = takeScreenShot(getWindow().getDecorView().getRootView(), "result");
-                if(result != null){
+                if (result != null) {
                     Toast.makeText(getApplicationContext(), "Comprovante salvo com sucesso!",
                             Toast.LENGTH_LONG).show();
-                } else{
+                } else {
                     Toast.makeText(getApplicationContext(), "Ocorreu um erro ao salvar o comprovante!",
                             Toast.LENGTH_LONG).show();
                 }
@@ -56,16 +62,17 @@ public class ValorTotalCotacao extends AppCompatActivity {
         });
     }
 
-    protected static File takeScreenShot(View view, String fileName){
+
+    protected static File takeScreenShot(View view, String fileName) {
         Date now = new Date();
-        CharSequence format =  DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+        CharSequence format = DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
         try {
-            String dirPath = view.getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/" + now + ".png";
+            String dirPath = view.getContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/" + now + ".png";
             File fileDir = new File(dirPath);
-            if(!fileDir.exists()){
-                 fileDir.mkdirs();
+            if (!fileDir.exists()) {
+                fileDir.mkdirs();
             }
-            String path = dirPath + "/" + fileName  + "-" + format + ".png";
+            String path = dirPath + "/" + fileName + "-" + format + ".png";
             //view.setDrawingCacheEnabled(true);
             Bitmap bitmap = getBitmapFromView(view);
             //view.setDrawingCacheEnabled(false);
@@ -86,23 +93,26 @@ public class ValorTotalCotacao extends AppCompatActivity {
         return null;
 
     }
-    private static  final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSION_STORAGE={
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSION_STORAGE = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE
 
     };
-    public static void verifyStoragePermission(Activity activity){
+
+    public static void verifyStoragePermission(Activity activity) {
         int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if(permission != PackageManager.PERMISSION_GRANTED){
+        if (permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(activity, PERMISSION_STORAGE, REQUEST_EXTERNAL_STORAGE);
         }
     }
-    public static Bitmap getBitmapFromView(View view)
-    {
+
+    public static Bitmap getBitmapFromView(View view) {
         Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         view.draw(canvas);
         return bitmap;
     }
+
 }
