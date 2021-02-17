e23a934804ba5cb22faa8a0619db259b0a983940
JVictor <joaovictorb.dasilva2003@gmail.com>
2021-02-16 21:05:52 -0400

    buscar todos os compromissos de uma determinada data

--- app/src/main/java/view/CompromissosAgendados.java
+++ app/src/main/java/view/CompromissosAgendados.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.corretagemapp.R;
+import com.example.corretagemapp.controllers.CompromissosController;
 
 public class CompromissosAgendados extends AppCompatActivity {
-
+    private CompromissosController controller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compromissos_agendados);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        controller = new CompromissosController(getBaseContext());
+        controller.getAllCompromissos();
     }
 }
