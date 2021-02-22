490e9f64914f730a4ebab8305703f0af3cb8d087
JVictor <joaovictorb.dasilva2003@gmail.com>
2021-02-22 12:08:49 -0400

    imprimir cotação dinamicamente

--- app/src/main/java/view/CotacaoAdapter.java
+++ app/src/main/java/view/CotacaoAdapter.java
@@ -2,11 +2,13 @@ package view;
 
 
 
+import android.content.Context;
 import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,12 +17,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.corretagemapp.R;
 import com.example.corretagemapp.models.CotacaoModel;
+import com.example.corretagemapp.models.CotacaoModelPreco;
 
 import java.util.List;
 
 public class CotacaoAdapter extends RecyclerView.Adapter<CotacaoAdapter.CotacaoViewHolder> {
     private final List<CotacaoModel> cotacoes;
-
     CotacaoAdapter(List<CotacaoModel> cotacoes){
         this.cotacoes = cotacoes;
     }
@@ -30,7 +32,7 @@ public class CotacaoAdapter extends RecyclerView.Adapter<CotacaoAdapter.CotacaoV
         View view = LayoutInflater.from(parent.getContext()).inflate(
                 R.layout.cotacao_card, parent, false
         );
-        return new CotacaoViewHolder(view);
+        return  new CotacaoViewHolder(view);
     }
 
 
@@ -44,22 +46,34 @@ public class CotacaoAdapter extends RecyclerView.Adapter<CotacaoAdapter.CotacaoV
         return cotacoes.size();
     }
      static class CotacaoViewHolder extends RecyclerView.ViewHolder{
+         private Context context;
          TextView idade;
-         TextView apartamento_preco;
-         TextView enfermagem_preco;
+         LinearLayout apartamento_title;
+         LinearLayout apartamento_preco;
+         LinearLayout enfermangem_title;
+         LinearLayout enfermagem_preco;
          private boolean isIdade = false;
+         TextView textViewDinamic;
          public CotacaoViewHolder(@NonNull View itemView) {
              super(itemView);
              idade = itemView.findViewById(R.id.idade);
+             apartamento_title = itemView.findViewById(R.id.title_apartamento);
              apartamento_preco = itemView.findViewById(R.id.preco_apartamento);
              enfermagem_preco = itemView.findViewById(R.id.preco_enfermagem);
-
+             enfermangem_title = itemView.findViewById(R.id.title_enfermagem);
          }
 
          public void bind(CotacaoModel cotacaoModel){
            idade.setText(cotacaoModel.getIdade());
-           apartamento_preco.setText(cotacaoModel.getListApartamentoPreco().get(0).getTitle());
-           enfermagem_preco.setText(cotacaoModel.getListEnfermagemPreco().get(0).getTitle());
+           List<CotacaoModelPreco> listApartamentoPreco = cotacaoModel.getListApartamentoPreco();
+           for(int count = 0; count < listApartamentoPreco.size(); count++){
+               textViewDinamic = new TextView(itemView.getContext().getApplicationContext());
+               textViewDinamic.setText(listApartamentoPreco.get(count).getTitle());
+               apartamento_title.addView(textViewDinamic);
+           }
+
+           //apartamento_preco.setText(cotacaoModel.getListApartamentoPreco().get(0).getTitle());
+           //enfermagem_preco.setText(cotacaoModel.getListEnfermagemPreco().get(0).getTitle());
          }
      }
 
