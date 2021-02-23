83a7364a92e27b50db64bf001e335ffad9265186
Your Name <you@example.com>
2021-02-22 22:35:42 -0400

    json ajustado v3

--- app/src/main/java/view/ValorTotalCotacao.java
+++ app/src/main/java/view/ValorTotalCotacao.java
@@ -97,7 +97,7 @@ public class ValorTotalCotacao extends AppCompatActivity {
         Date now = new Date();
         CharSequence format = DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
         try {
-            String dirPath = view.getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/" + now + ".png";
+            String dirPath = view.getContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/" + now + ".png";
             File fileDir = new File(dirPath);
             if (!fileDir.exists()) {
                 fileDir.mkdirs();
