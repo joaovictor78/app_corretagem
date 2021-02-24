0e51a2df810fa40b00f590c937179e1e18cca91d
JVictor <joaovictorb.dasilva2003@gmail.com>
2021-02-24 18:18:44 -0400

    correção do bug ao adicionar imagem a galeria - ok

--- app/src/main/java/view/ValorTotalCotacao.java
+++ app/src/main/java/view/ValorTotalCotacao.java
@@ -8,13 +8,17 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -34,6 +38,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -186,8 +191,8 @@ public class ValorTotalCotacao extends AppCompatActivity {
         printButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                File result = takeScreenShot(getWindow().getDecorView().getRootView(), getApplicationContext(), "result");
-                if (result != null) {
+                boolean result = takeScreenShot(getWindow().getDecorView().getRootView(), "result");
+                if (result != false) {
                     Toast.makeText(getApplicationContext(), "Comprovante salvo com sucesso!",
                             Toast.LENGTH_LONG).show();
                 } else {
@@ -200,26 +205,28 @@ public class ValorTotalCotacao extends AppCompatActivity {
 
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    protected static File takeScreenShot(View view, Context context, String fileName) {
+    protected static Boolean takeScreenShot(View view, String fileName) {
         Date now = new Date();
         CharSequence format = DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+        final ContentResolver resolver = view.getContext().getContentResolver();
         try {
-            String dirPath = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath() + "/" + now + ".jpeg";
+            String dirPath = Environment.DIRECTORY_PICTURES + "/" + now + ".png";
             File fileDir = new File(dirPath);
             if (!fileDir.exists()) {
                 fileDir.mkdirs();
             }
             String path = dirPath + "/" + fileName + "-" + format + ".jpeg";
+            final ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, path);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE,"image/jpeg");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, dirPath);
+            final Uri contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            Uri uri = resolver.insert(contentUri, contentValues);
+            OutputStream stream = resolver.openOutputStream(uri);
             Bitmap bitmap = getBitmapFromView(view);
-            File imageFile = new File(path);
-            FileOutputStream fileOutputStream = new FileOutputStream(imageFile);
             int quality = 100;
-            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, fileOutputStream);
-            fileOutputStream.flush();
-            fileOutputStream.close();
-            return imageFile;
-
-
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream);
+            return true;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -228,7 +235,6 @@ public class ValorTotalCotacao extends AppCompatActivity {
         return null;
 
     }
-
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSION_STORAGE = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
